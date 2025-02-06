7f467d0b8fbb6014da8a4374d11ceea34dfabf31
fbansept <bansept.franck@gmail.com>
2025-02-06 15:31:30 +0100

    introduction bug

--- src/main/java/edu/fbansept/td/CoursFilRougeSpring3TdApplication.java
+++ src/main/java/edu/fbansept/td/CoursFilRougeSpring3TdApplication.java
@@ -9,11 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoursFilRougeSpring3TdApplication {
 
-    /**
-     * Main method.
-     *
-     * @param args Not used.
-     */
+
     public static void main(String[] args) {
         SpringApplication.run(CoursFilRougeSpring3TdApplication.class, args);
         System.out.println("instruction mal indentée");
