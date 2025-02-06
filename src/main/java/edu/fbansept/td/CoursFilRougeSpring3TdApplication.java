705add58e28e7599acb4444e876d0c1f9310fd63
fbansept <bansept.franck@gmail.com>
2025-02-06 15:28:07 +0100

    fix doc

--- src/main/java/edu/fbansept/td/CoursFilRougeSpring3TdApplication.java
+++ src/main/java/edu/fbansept/td/CoursFilRougeSpring3TdApplication.java
@@ -3,9 +3,17 @@ package edu.fbansept.td;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class.
+ */
 @SpringBootApplication
 public class CoursFilRougeSpring3TdApplication {
 
+    /**
+     * Main method.
+     *
+     * @param args Not used.
+     */
     public static void main(String[] args) {
         SpringApplication.run(CoursFilRougeSpring3TdApplication.class, args);
     }
